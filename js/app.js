32a5d3cfc4efb49ae0741984f4a4548a3de8a099
Homer <hhonasan@sorsogonstatecollege.edu.ph>
2021-04-27 16:45:48 +0800

    Validating Fields

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // Variables
-const sendBtn = document.getElementById('sendBtn');
+const sendBtn = document.getElementById('sendBtn'), 
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message');
 
 
 
@@ -10,6 +13,11 @@ eventListeners();
 function eventListeners() {
 	//App init
 	document.addEventListener('DOMContentLoaded', appInit);
+
+    // Validate the forms
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
 
 
@@ -19,4 +27,39 @@ function eventListeners() {
 function appInit() {
 	// Disable the send button on load
 	sendBtn.disabled = true;
+}
+
+// Validate the fields
+function validateField() {
+    let errors;
+
+    // Validate the length of the field
+    validateLength(this);
+
+    // Validate the email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+}
+// Validate the length of the fields
+function validateLength(field) {
+    if(field.value.length > 0 ) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+// Validate email (checks for @ in the value )
+function validateEmail(field) {
+    let emailText = field.value;
+    // check if the emailText contains the @ sign
+    if(emailText.indexOf('@') !== -1) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
 }
